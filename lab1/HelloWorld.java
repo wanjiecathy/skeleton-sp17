9acbebd760f0685dde3bc7f54dd06ee5c33dd12e
wanjie <wanjiezhang.cathy@gmail.com>
2018-01-17 15:21:02 -0500

    completed lab1

---
+++ lab1/HelloWorld.java
@@ -0,0 +1,7 @@
+class HelloWorld {
+
+    /** Must be provided an integer as a command line argument ARGS. */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
